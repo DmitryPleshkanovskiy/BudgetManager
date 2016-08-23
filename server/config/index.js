5d03e64ce9259e33e753f767ba87df469ff0eb6d
Dmitry Pleshkanovskiy <dmitry.pleshkanovskiy@gmail.com>
2016-08-23 17:15:27 +0300

    mvp: Removed server v1.0, moved port to config file

---
+++ server/config/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Nick on 21.08.2016.
+ */
+
+module.exports = {
+	port: 5000
+};
--- server/server.js
+++ server/server.js
@@ -1,87 +1,13 @@
 /**
- * Created by dmitry on 13.03.16.
+ * Created by dmitry on 15.05.16.
  */
 
-'use strict';
+//var debug        = require('debug');
+var app          = require('./app');
+var config       = require('./config');
 
-var express = require('express');
-var mongoClient = require('mongodb').MongoClient;
-var engines = require('consolidate');
-var bodyParser = require('body-parser');
-var path = require('path');
-var morgan = require('morgan');
-var assert = require('assert');
+app.set('port', process.env.PORT || config.port);
 
-var app = express();
-
-app.engine('html', engines.nunjucks);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'html');
-
-app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-mongoClient.connect('mongodb://budmanclient:SFHdghse56w457@ds011492.mlab.com:11492/budman', function (err, db) {
-
-    require('./api/api.js')(app, db);
-
-    app.get('/nun', function (req, res) {
-        db.collection('items').find({}).sort({date: 1}).toArray(function (err, docs) {
-            res.render('index', {items: docs});
-        });
-    });
-
-    app.get('/newitem', function (req, res, next) {
-        res.render('newitem', {});
-    });
-
-    app.post('/newitem', function (req, res, next) {
-        var date = req.body.date;
-        var sum = req.body.sum;
-        var description = req.body.description;
-        
-        //TODO add data validation
-        
-        db.collection('items').insertOne(
-            {'date': date, 'sum': sum, 'description': description},
-            function (err, r) {
-                assert.equal(null, err);
-                res.redirect('/');
-        });
-    });
-
-    app.get('/update/:id', function (req, res, next) {
-        var id = req.params.id;
-        //console.log(id);
-        //var id = 'Hello!';
-        res.status(200).send(id);
-    });
-
-    app.get('/nun/*', function (req, res, next) {
-        res.redirect('/nun');
-    });
-
-
-    app.use(express.static('client/bower_components'));
-
-    app.use(express.static('client'));
-
-    app.get('/*', function (req, res, next) {
-        res.sendFile(path.join(__dirname, '../client', '/index.html'));
-    });
-});
-
-
-
-
-//app.use(express.static('client'));
-
-/*app.get('/!*', function (req, res, next) {
-    res.sendFile(path.join(__dirname, '../client', '/index.html'));
-});*/
-
-
-app.listen(5000, function () {
-   console.log("App is running on port 3000");
+var server = app.listen(app.get('port'), function(){
+   console.log('Express server listening on port ' + server.address().port);
 });
