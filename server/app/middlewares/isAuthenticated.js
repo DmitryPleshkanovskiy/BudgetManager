a46f7479ad7e6012529021fb389e135e6f8a5e83
Dmitry Pleshkanovskiy <dmitry.pleshkanovskiy@gmail.com>
2016-11-21 23:16:36 +0200

    feature-auth: added api user authentication and authorization

--- server/app/middlewares/isAuthenticated.js
+++ server/app/middlewares/isAuthenticated.js
@@ -1,3 +1,48 @@
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const config = require('../../config')
+
 const isAuthenticated = function(req, res, next) {
-    next();
-}
+    if (passport.authenticate('jwt', { session: false})) {
+        var token = getToken(req.headers);
+        if (token) {
+            //var decoded = jwt.decode(token, config.auth.secret);
+            jwt.verify(token, config.auth.secret, function(err, decoded) {
+                if (err) {
+                    return res.status(403).send({success: false, msg: 'Authentication failed. Broken token.'});
+                } else {
+                    User.findOne({
+                    name: decoded.name
+                    }, function(err, user) {
+                        if (err) throw err;
+                
+                        if (!user) {
+                            return res.status(403).send({success: false, msg: 'Authentication failed. User not found.'});
+                        } else {
+                            next();
+                            //res.json({success: true, msg: 'Welcome in the member area ' + user.name + '!'});
+                        }
+                    });
+                }
+            });
+        } else {
+            return res.status(403).send({success: false, msg: 'No token provided.'});
+        }
+    }
+};
+
+getToken = function (headers) {
+  if (headers && headers.authorization) {
+    var parted = headers.authorization.split(' ');
+    if (parted.length === 2) {
+      return parted[1];
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+};
+
+module.exports = isAuthenticated;
--- server/app/routes/api/user.js
+++ server/app/routes/api/user.js
@@ -7,6 +7,10 @@
 const router = require('express').Router();
 
 const User = require('../../models/user');
+const jwt = require('jwt-simple');
+const config = require('../../../config');
+
+const isAuthenticated = require('../../middlewares/isAuthenticated');
 
 router.get('/',  (req, res, next) => {
     res.status(200).send('get all users');
@@ -28,6 +32,30 @@ router.post('/signup',  (req, res, next) => {
             res.json({success: true, msg: 'Successfully created new user.'});
         })
     }
+});
+
+router.post('/authenticate', function(req, res) {
+    User.findOne({
+        email: req.body.email
+    }, function(err, user) {
+        if (err) { throw err; }
+        if (!user) {
+            res.send({success: false, msg: 'Authentication failed. User not found.'});
+        } else {
+            user.comparePassword(req.body.password, function(err, isMatch) {
+                if (isMatch && !err) {
+                    const token = jwt.encode(user, config.auth.secret);
+                    res.json({success: true, token: 'JWT ' + token});
+                } else {
+                    res.send({success: false, msg: 'Authentication failed. Wrong password.'});
+                }
+            });
+        }
+    });
+})
+
+router.get('/me', isAuthenticated, function(req, res) {
+    res.send('me');
 });
 
 router.get('/:id', (req, res, next) => {
